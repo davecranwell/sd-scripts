36ec38e5d4ba4e94fe9d41c99d364a3206a39f41
Dave Cranwell <xcession@xcessionsites.com>
2025-04-02 21:07:50 +0100

    Updates permitted keys

--- training_api.py
+++ training_api.py
@@ -200,6 +200,7 @@ def create_app(session_manager=None):
             'network_dim', 'lr_scheduler', 'metadata_author', 'bucket_reso_steps', 'max_bucket_reso', 'sample_every_n_epochs', 'metadata_title',
             'flip_aug', 'sample_prompts', 'resolution', 'output_name', 'upload_url', 'full_bf16', 'network_module', 'metadata_license', 'optimizer_type', 'fp8_base', 'optimizer_args', 'unet_lr', 'sdxl_cache_text_encoder_outputs', 'metadata_description', 'train_batch_size', 'min_bucket_reso', 'sample_every_n_steps', 'keep_tokens', 'seed', 'metadata_tags', 'max_train_epochs', 'text_encoder_lr', 'clip_skip', 'network_alpha', 'full_fp16', 'trigger_word', 'sample_sampler', 'training_images_url',
             'image_repeats',
+            'save_every_n_epochs',
             'id',
             'webhook_url',
             'civitai_key',
